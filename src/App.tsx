b60eec6ed18e8160e9340dfbb76686559732b464
Arina Siedina <orynasiedina@gmail.com>
2023-11-12 21:47:31 +0100

    Adding Toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import { AuthLayout, SigninForm, SignupForm } from "./_auth";
-import RootLayout from "./_root/RootLayout";
-import { Home } from "./_root/pages";
-import "./global.css";
-import { Routes, Route } from "react-router-dom";
-import { Toaster } from "@/components/ui/toaster"
+import { AuthLayout, SigninForm, SignupForm } from './_auth';
+import RootLayout from './_root/RootLayout';
+import { Home } from './_root/pages';
+import './global.css';
+import { Routes, Route } from 'react-router-dom';
+import { Toaster } from '@/components/ui/toaster';
 
 const App = () => {
   return (
-    <main className="flex h-screen">
+    <main className='flex'>
+      <Toaster />
       <Routes>
         {/*public routes */}
         <Route element={<AuthLayout />}>
-          <Route path="/sign-in" element={<SigninForm />} />
-          <Route path="/sign-up" element={<SignupForm />} />
+          <Route path='/sign-in' element={<SigninForm />} />
+          <Route path='/sign-up' element={<SignupForm />} />
         </Route>
         {/*private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
         </Route>
       </Routes>
-      <Toaster />
     </main>
   );
 };
