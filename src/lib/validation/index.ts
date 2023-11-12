e91e2efc5e8151d840e01f7d62e4d0fbc8cf0cb0
Arina Siedina <orynasiedina@gmail.com>
2023-11-12 22:38:03 +0100

    added validation for sign-in

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,9 +1,13 @@
 import * as z from "zod"
 
 export const SignupValidation = z.object({
-    name: z.string().min(2,{message:'Too short'}).max(16, {message:'Too long'}),
-    username: z.string().min(2,{message:'Too short'}).max(16, {message:'Too long'}),
+    name: z.string().min(2,{message:'Name should be at least 2 characters'}),
+    username:z.string(),
+    email: z.string().email({message:'Invalid email'}),
+    password: z.string().min(8,{message:'Password should be at least 8 characters'}),
+  })
+  
+  export const SigninValidation = z.object({
     email: z.string().email({message:'Invalid email'}),
     password: z.string().min(8,{message:'Password must be at least 8 characters'}),
   })
-  
