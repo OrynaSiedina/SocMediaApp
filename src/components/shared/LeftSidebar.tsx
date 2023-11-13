bbfb48945b3487741a12c45e3b36bc6ad349a735
Arina Siedina <orynasiedina@gmail.com>
2023-11-13 16:08:05 +0100

    Left-side bar

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { useSignOutAccount } from '@/lib/react-query/quariesAndMutations.ts';
+import { useUserContext } from '@/context/AuthContext.tsx';
+import { useEffect } from 'react';
+import { Button } from '@/components/ui/button.tsx';
+import { sidebarLinks } from '@/constants';
+import { INavLink } from '@/types';
 
 const LeftSidebar = () => {
-  return (
-    <div>LeftSidebar</div>
-  )
-}
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+  const navigate = useNavigate();
+  const { user } = useUserContext();
+  const { pathname } = useLocation();
 
-export default LeftSidebar
+  useEffect(() => {
+    if (isSuccess) navigate(0);
+  }, [isSuccess]);
+  return (
+    <>
+      <nav className='leftsidebar'>
+        <div className='flex flex-col gap-11'>
+          <Link to='/' className='flex gap-3 items-center'>
+            <img src='/assets/images/logo.svg' alt='logo' className='w-full' />
+          </Link>
+
+          <Link to={`/profile/${user.id}`} className='flex items-center gap-3'>
+            <img
+              src={user.imageUrl || '/assets/images/person_icon.png'}
+              alt='profile'
+              className='h-14 rounded-full'
+            />
+            <div className='flex flex-col'>
+              <p className='body-bold'>{user.name}</p>
+              <p className='small-regular text-light-3'>@{user.username}</p>
+            </div>
+          </Link>
+          <ul className='flex flex-col gap-6'>
+            {sidebarLinks.map((link: INavLink) => {
+              const isActive = pathname === link.route;
+              return (
+                <li
+                  className={`leftsidebar-link group ${
+                    isActive && 'bg-primary-500'
+                  }`}
+                  key='link.label'
+                >
+                  <NavLink
+                    to={link.route}
+                    className='flex gap-4 items-center p-4'
+                  >
+                    <img
+                      src={link.imgURL}
+                      alt={link.label}
+                      className={`group-hover:invert-white 
+                       ${isActive && 'invert-white'}`}
+                    />
+                    {link.label}
+                  </NavLink>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <Button
+          variant='ghost'
+          className='shad-button_ghost'
+          onClick={() => signOut()}
+        >
+          <img src='/assets/icons/logout.svg' alt='logout' />
+          <p className='base-medium'>Logout</p>
+        </Button>
+      </nav>
+    </>
+  );
+};
+
+export default LeftSidebar;
