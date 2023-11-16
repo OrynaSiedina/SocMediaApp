0c130e4cc79d7e1f972eb698137802c06aea5ba7
Arina Siedina <orynasiedina@gmail.com>
2023-11-16 22:47:42 +0100

    Home with Post cards

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,7 +1,32 @@
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import { Loader } from 'lucide-react';
+import { useGetRecentPosts } from '@/lib/react-query/quariesAndMutations.ts';
+import { Models } from 'appwrite';
+import PostCard from '@/components/shared/PostCard.tsx';
 
-export default Home
+const Home = () => {
+  const {
+    data: posts,
+    isPending: isPostLoading,
+    isError: isErrorPosts,
+  } = useGetRecentPosts();
+  return (
+    <div className='flex flex-1'>
+      <div className='home-container'>
+        <div className='home-posts'>
+          <h2 className='h3-bold md:h-2-bold text-left w-full'>Home Feed</h2>
+        </div>
+        {isPostLoading && !posts ? (
+          <Loader />
+        ) : (
+          <ul className='flex flex-col lfex-1 gap-9 w-full'>
+            {posts?.documents.map((post: Models.Document) => (
+              <PostCard key={post.$id} post={post} />
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
